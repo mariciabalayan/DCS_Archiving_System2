bd4ac41feddfd00fec52913e8772b98f23f5a7b6
Carl <carl.earvin.yu@gmail.com>
2013-02-09 16:41:19 +0800

    remove whitespace + comma

--- DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
+++ DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
@@ -69,6 +69,8 @@ def scanpage2(request):
         title= request.POST.get('title')
         faculty= request.POST.get('faculty')
         pages= request.POST.get('pages')
+        faculty= faculty.replace(' ', "")
+        faculty= faculty.replace(',', "")
 #        if (title!= None and title != '') and (faculty!= None and faculty!= '') and pages!='' and pages!= None:
         return render_to_response('scanpage2.html', { 'user': request.user, 'faculty_list': users_list, 'title':title, 'pages':pages, 'faculty':faculty, 'state':state}, context_instance=RequestContext(request))
         
