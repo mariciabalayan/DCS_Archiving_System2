881839c157a4c284188a8a0b91823bc32917bc36
Marc Lopez <molave.35@gmail.com>
2013-02-09 16:43:14 +0800

    Remove scanner import

--- DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
+++ DCSArchivingSystem/DCSArchivingSystem/testapp/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import RequestContext
-import scanner
 from django.contrib.auth.models import User
 from models import Faculty, File, Log
 from forms import ScanForm
