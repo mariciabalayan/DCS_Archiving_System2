4161b5923d4bbd4de052621cd1c1a453a1cff7d4
John Smith Paraggua <smitparaggua@gmail.com>
2013-03-03 16:28:42 +0800

    modified forms.py to be consistent with scanpage

--- DCSArchivingSystem/DCSArchivingSystem/testapp/forms.py
+++ DCSArchivingSystem/DCSArchivingSystem/testapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from models import Faculty
+from models import Transaction
 class ScanForm(forms.Form):
-    title= forms.CharField(max_length= 100)
+    #title= forms.CharField(max_length= 100)
+    transactions= forms.ChoiceField(choices=Transaction.objects.all())
     faculty= forms.CharField(max_length= 100)
     pages= forms.IntegerField
